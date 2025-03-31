f65beed14107869a2f79fdb67b8e6318ce427f6d
Connor Harris <97750331+The-Real-Connor-Harris@users.noreply.github.com>
2025-03-31 19:50:57 -0400

    Cleaned up users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -12,42 +12,6 @@ router.get('/users', async (req, res, next) => {
     }
 });
 
-// router.get('/books', async (req, res, next) => {
-//     try {
-//         const books = await getAllBooks();
-//         res.json(books);
-//     } catch (err) {
-//         next(err);
-//     }
-// });
-
-// router.get('/books/:bookId', async (req, res, next) => {
-//     try {
-//         const book = await getOneBook(+req.params.bookId);
-//         res.json(book);
-//     } catch (err) {
-//         next(err);
-//     }
-// });
-
-// router.post('/books', async (req, res, next) => {
-//     try {
-//         const book = await createBook(req.body);
-//         res.status(201).json(book);
-//     } catch (err) {
-//         next(err);
-//     }
-// });
-
-router.get('/users', async (req, res, next) => {
-    try {
-        const users = await getAllUsers();
-        res.json(users);
-    } catch (err) {
-        next(err);
-    }
-});
-
 router.get('/users/:userId', async (req, res, next) => {
     try {
         const user = await getOneUser(+req.params.userId);
