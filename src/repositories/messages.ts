c21a530e35ed113d7758d45e8549706cc65e631c
MrOtonashi <dany-1765@outlook.com>
2025-03-31 20:13:01 -0400

    fixed messages

--- src/repositories/messages.ts
+++ src/repositories/messages.ts
@@ -1,67 +1,43 @@
 import { db } from "../db/db.js";
 
 export type Message = {
-    messageId: number;
-    content: string;
-    tags: string[];
+    messageID: number;
+    title: string;
+    body: string;
+    senderID: number;
+    createdAt: Date;
+    updatedAt: Date;
 };
 
 export async function getAllMessages(): Promise<Message[]> {
     const messages = await db
         .selectFrom('message')
-        .select(['messageId', 'content'])
+        .select(['messageID', 'title', 'body', 'senderID', 'createdAt', 'updatedAt'])
         .execute();
-    const tags = await db
-        .selectFrom('tag')
-        .select(['tagName', 'messageId'])
-        .execute();
-    const allMessages: Message[] = [];
-    for (const message of messages) {
-        const messageTags = tags.filter(t => t.messageId === message.messageId);
-        allMessages.push({
-            ...message,
-            tags: messageTags.map(t => t.tagName),
-        });
-    }
-    return allMessages;
+    return messages;
 }
 
 export async function getOneMessage(messageId: number): Promise<Message> {
     const message = await db
         .selectFrom('message')
-        .select(['messageId', 'content'])
-        .where('messageId', '=', messageId)
+        .select(['messageID', 'title', 'body', 'senderID', 'createdAt', 'updatedAt'])
+        .where('messageID', '=', messageId)
         .executeTakeFirstOrThrow();
-    const tags = await db
-        .selectFrom('tag')
-        .select(['tagName'])
-        .where('messageId', '=', messageId)
-        .execute();
-    return {
-        ...message,
-        tags: tags.map(t => t.tagName),
-    };
+    return message;
 }
 
-export async function createMessage(message: Omit<Message, 'messageId'>): Promise<Message> {
+export async function createMessage(message: Omit<Message, 'messageID'>): Promise<Message> {
     const messageId = await db.transaction().execute(async (trx) => {
         const createdMessage = await trx
             .insertInto('message')
-            .columns(['content'])
-            .values({ content: message.content })
-            .returning(['messageId'])
+            .columns(['title', 'body', 'senderID'])
+            .values({ title: message.title, body: message.body, senderID: message.senderID })
+            .returning(['messageID'])
             .executeTakeFirstOrThrow();
-        if (message.tags.length > 0) {
-            await trx
-                .insertInto('tag')
-                .columns(['messageId', 'tagName'])
-                .values(message.tags.map(t => ({ messageId: createdMessage.messageId, tagName: t })))
-                .executeTakeFirstOrThrow();
-        }
-        return createdMessage.messageId;
+        return createdMessage.messageID;
     });
     return {
         ...message,
-        messageId,
+        messageID: messageId,
     };
 }
