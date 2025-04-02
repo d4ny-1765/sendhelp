cd6d578582a3f3bcda48c85fe48c935fc1f79803
MrOtonashi <dany-1765@outlook.com>
2025-04-02 08:35:58 -0400

    fixed index.ts routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { migrateToLatest } from "./db/migrate.js";
-import bookRouter from './routes/users.js';
+import usersRouter from './routes/users.js';
+import roomRouter from './routes/rooms.js';
+import messageRouter from './routes/messages.js';
+import topicRouter from './routes/topic.js';
 
 await migrateToLatest();
 
@@ -8,7 +11,13 @@ export const app = express();
 
 app.use(express.json());
 
-app.use('/api/v1', bookRouter);
+app.use('/api/v1', usersRouter);
+
+app.use('/api/v1', roomRouter);
+
+app.use('/api/v1', messageRouter);
+
+app.use('/api/v1', topicRouter);
 
 if (process.env.APP_ENV !== 'test') {
     app.listen(3000, () => console.log('Listening on port 3000'));
