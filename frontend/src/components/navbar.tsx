a47c07176d36ea8abb1c0d4f9ac74431a011b44e
NightShady2002 <bjoinseth74@gmail.com>
2025-04-23 21:10:44 -0400

    profile icon

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -4,6 +4,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { Link as RouterLink } from 'react-router-dom';
+import { Avatar, IconButton } from '@mui/material';
+import userIcon from '../assets/user.png';
 
 const Navbar: React.FC = () => {
   return (
@@ -24,9 +26,18 @@ const Navbar: React.FC = () => {
         </Typography>
 
         {/* Navigation buttons */}
+        <IconButton component={RouterLink} to="/profile" sx={{ p: 0, ml: 2 }}>
+  <Avatar
+    src={userIcon}
+    alt="Profile"
+    sx={{ width: 36, height: 36 }}
+  />
+</IconButton>
+
         <Button color="inherit" component={RouterLink} to="/login">
           Login
         </Button>
+        
       </Toolbar>
     </AppBar>
   );
